af2159de939b6be23bcd82763adc2721eb032a96
nknilandu <nknilandu@gmail.com>
2026-06-04 23:41:05 +0600

    added get method for my-tickets routes and installed firebase-admin and added firebase-admin-sdk andd added middleware

--- index.js
+++ index.js
@@ -5,11 +5,51 @@ const app = express();
 const port = process.env.PORT || 3021;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-// mongodb connection
+// firebase admin setup
+const admin = require("firebase-admin");
+const serviceAccount = require("./support-hub-ai-firebase-admin-sdk.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// ============= generate Ticket Number ================
+const generateTicketNumber = () => {
+  const timePart = Date.now().toString(16).slice(-4).toUpperCase();
+  const randomPart = crypto.randomBytes(2).toString("hex").toUpperCase();
+  return `TCK-${timePart}${randomPart}`;
+};
+
+//=============  verifyFirebaseToken =================
+const verifyFirebaseToken = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res
+      .status(401)
+      .send({ message: "Unauthorized Access – Authentication required" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res
+      .status(401)
+      .send({ message: "Unauthorized Access – Authentication required" });
+  }
+
+  // verify token
+  try {
+    const tokenInfo = await admin.auth().verifyIdToken(token);
+    req.user = tokenInfo;
+    next();
+  } catch {
+    return res
+      .status(401)
+      .send({ message: "Unauthorized Access – Authentication required" });
+  }
+};
+
+// ======================== mongodb connection ============================
 const uri =
   "mongodb+srv://nknilandu:8ryfXnfOq3jLIZWc@cluster-support-hub.idvmsjp.mongodb.net/?appName=Cluster-Support-Hub";
 
@@ -25,13 +65,6 @@ const client = new MongoClient(uri, {
 app.get("/", (req, res) => {
   res.send("Successfully Connected to SupportHub");
 });
-
-const generateTicketNumber = () => {
-  const timePart = Date.now().toString(16).slice(-4).toUpperCase();
-  const randomPart = crypto.randomBytes(2).toString("hex").toUpperCase();
-
-  return `TCK-${timePart}${randomPart}`;
-};
 
 async function run() {
   try {
@@ -143,10 +176,10 @@ async function run() {
 
     // ================= CREATE TICKET =================
 
-    app.post("/tickets", async (req, res) => {
+    app.post("/tickets", verifyFirebaseToken, async (req, res) => {
       try {
         const body = req.body;
-on
+
         if (!body?.uid || !body?.email || !body?.aiResult) {
           return res.status(400).send({
             success: false,
@@ -169,6 +202,70 @@ on
           id: result.insertedId,
           ticketId: result.ticketId,
           ticketNumber: ticketNumber,
+        });
+      } catch (error) {
+        return res.status(500).send({
+          success: false,
+          message: error.message,
+        });
+      }
+    });
+
+    // ================= GET MY TICKETS =================
+
+    app.get("/tickets/my-tickets", verifyFirebaseToken, async (req, res) => {
+      try {
+        const { search, status, priority, category } = req.query;
+
+        const query = {
+          email: req.user.email,
+        };
+
+        // Search
+        if (search) {
+          query.$or = [
+            {
+              ticketNumber: {
+                $regex: search,
+                $options: "i",
+              },
+            },
+            {
+              "aiResult.ticketTitle": {
+                $regex: search,
+                $options: "i",
+              },
+            },
+          ];
+        }
+        // Status
+        if (status) {
+          query.status = status;
+        }
+        // Category
+        if (category) {
+          query["aiResult.category"] = category;
+        }
+
+        // Priority
+        if (priority) {
+          query["aiResult.states"] = {
+            $elemMatch: {
+              title: { $regex: /^priority$/i },
+              value: { $regex: new RegExp(`^${priority}$`, "i") },
+            },
+          };
+        }
+
+        const result = await tickets
+          .find(query)
+          .sort({ createdAt: -1 })
+          .toArray();
+
+        return res.send({
+          success: true,
+          count: result.length,
+          data: result,
         });
       } catch (error) {
         return res.status(500).send({
